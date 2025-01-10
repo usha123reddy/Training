458979c565b59c9d62cac9cd53e858b73c47cfb5
usha123reddy <ushareddyvemireddy@gmail.com>
2025-01-10 10:30:39 +0530

    Sending msg

--- functions.js
+++ functions.js
@@ -59,9 +59,68 @@ x(10); */
     console.log("why");
 x(); */
 //!{} if not used then it will print but not in order
-let x=_=>{
+/* let x=_=>{
     console.log("HI");
     console.log("Hello");
     console.log("why");
 }
+x(); */
+
+/* //!return function
+function test(a,b){
+    console.log("hi");
+    console.log("hhvj");//explicit return
+    return a+b;
+    console.log("hello");
+}
+let x=test(10,12);
+console.log(x);
+
+let y=(a,b)=>a+b;//implicit return
+let z=y(10,20);
+console.log(z);
+
+let p=(a,b)=>{return a+b};//explicit return
+let q=p(90,20);
+console.log(q); */
+
+
+//!Higher order function
+/* function hof(a){
+    return a;
+}
+let m=hof("usha");
+let n=hof(true);
+let w=hof(function(){return "hello i am a call back function"});
+console.log(m);
+console.log(n);
+console.log(w); */
+/* function hof(a){
+    return a;
+}
+let w=hof(function(a,b){return a+b});
+console.log(w);
+ */
+/* //!callback function
+function parent(callback){
+    return callback; 
+}
+let x=parent(add(5,5));
+console.log(x);
+
+function add(a,b){
+    return a+b; 
+}
+/* console.log(add(5,5)); */
+let a=10;
+let b="sai";
+function x(){
+    var user="usha";
+    let company="MNC";
+    const sal=50000;
+    console.log(company);
+    console.log(user);
+    console.log(sal);
+    console.log(a,b);
+};
 x();
---
+++ programs.js
@@ -0,0 +1,19 @@
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=5;j++){
+        document.writeln("*");
+    }
+    document.write("<br>");
+}
+
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=5;j++){
+        if(i>=j){
+            document.writeln("*");
+        }else{
+            document.writeln("&nbsp;");
+        }
+        
+    }
+    document.write("<br>");
+}
+
