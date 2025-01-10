f69abf52232b5bff87b0939b06f11b7ff930a256
usha123reddy <ushareddyvemireddy@gmail.com>
2025-01-10 10:33:59 +0530

    Sending msg

--- programs.js
+++ programs.js
@@ -17,3 +17,17 @@ for(let i=1;i<=5;i++){
     document.write("<br>");
 }
 
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=5;j++){
+        if(i==j ||i+j==6){
+            document.writeln("*");
+        }else{
+            document.writeln("&nbsp;");
+        }
+        
+    }
+    document.write("<br>");
+}
+
+
+
