8890c339003bac61daa3c240276bc47edc70e955
usha123reddy <ushareddyvemireddy@gmail.com>
2025-01-24 12:56:40 +0530

    Sending msg

--- dom.js
+++ dom.js
@@ -3,6 +3,47 @@
 /* let element =document.getElementById("demo");
 element.innerText="G+";
 console.log(element); */
-let element =document.getElementById("test");
+/* let element =document.getElementById("test");
 element.innerHtml="<h1>Header</h1>";
+console.log(element); */
+/* let element =document.getElementsByClassName("test");
 console.log(element);
+console.log(Array.isArray(element));
+element[0].innerText="hello";
+element[0].style.backgroundColor="tomato";
+//!spread operator:it will take out each and every value from the orginal array and store it inside one more new array(pure array).
+//?syntax:-[...variable];
+let x=[...element];
+console.log(x,Array.isArray(x));
+x.map(element=>{
+    element.style.backgroundColor="green";
+}) */
+/* let ele=document.getElementById("formdata");
+console.log(ele,Array.isArray(ele));
+[...ele].map(inp=>{
+    console.log(inp,value);
+}) */
+/* let elem=document.querySelector("#mru");
+    console.log(elem);
+let ele=document.querySelectorAll(".test");
+console.log(ele); */
+
+/* let btn=document.querySelector("#btn"); */
+//!addEventListener(:event",()=>{})
+/*     btn.addEventListener("click",()=>{
+    console.log("button clicked")
+    alert("hi");
+    btn.style.backgroundColor="green"
+}); */
+/* btn.addEventListener("dbclick",()=>{
+    console.log("double clicked");
+
+
+}) */
+/* let divBlock=document.getElementById("divBlock");
+divBlock.addEventListener("mouseover",()=>{
+    document.style.backgroundColor="green";
+    divBlock.style.backgroundColor="pink";
+}) */
+
+    
