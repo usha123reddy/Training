7617a5304868a0cb4d7890726c1289fa2e290707
usha123reddy <ushareddyvemireddy@gmail.com>
2025-01-25 12:56:31 +0530

    Sending msg

--- dom.js
+++ dom.js
@@ -45,5 +45,41 @@ divBlock.addEventListener("mouseover",()=>{
     document.style.backgroundColor="green";
     divBlock.style.backgroundColor="pink";
 }) */
+/* let bgColor=document.querySelectorAll(".bgColor");
+console.log(bgColor);
+[...bgColor].map((element)=>{
+    element.addEventListener("mouseover",()=>{
+       
+        element.style.backgroundColor="element.innerText";
+    })
+})
+ */
+/* let ele=document.createElement("hi");
+ele.innerText="why";
+ele.setAttribute("id","demo");
+//ele.id="demo";
+//ele.removeAttribute("id");
+console.log(ele);
+let image=document.createElement("img");
+image.src="https://th.bing.com/th/id/OIP.uHaqRdiMzWSMCR2LzsmhtQHaEZ?w=254&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7";
+console.log(image);
+document.body.appendChild(ele);
+document.body.appendChild(image);
+ */
+let form=document.querySelector("form");
+let username=document.getElementById("uname");
+let mail=document.getElementById("umail");
 
-    
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    //console.log("form sub");
+    let uName=username.value;
+    console.log(uName);
+    let uEmail=mail.value;
+    console.log(uEmail);
+    let userDetails={
+        uName,uEmail
+    }
+    console.log(userDetails);
+    localStorage.setItem("userData",JSON.stringify(userDetails));
+})
